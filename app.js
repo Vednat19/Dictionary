b0ed7b9a160b42d18a151892da4c565d0667065e
Vedant Sharma <thisisvadantsharma.05@gmail.com>
2026-07-10 23:47:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click" , async () =>{
+    let input = document.querySelector("input").value.trim();
+    if (input === "") {
+        document.querySelector("#para").innerText = "Please enter a word.";
+        return;
+    }
+    let result = await get(input);
+     if (result) {
+        show(result);
+    } else {
+        document.querySelector("#para").innerText = "Word not found!";
+    }
+});
+
+function show(result){
+    let p = document.querySelector("#para");
+    p.innerText = "";
+    let definitions = result[0].meanings[0].definitions;
+     for (let i = 0; i < definitions.length; i++) {
+        p.innerText += `${i + 1}. ${definitions[i].definition}\n\n`;
+    }   
+}
+
+async function get(word){
+    try {
+        let data = await axios.get(url+word);
+        return data.data;
+    } catch (e) {
+        console.log(e);
+         return null;
+    }
+};
+
+// Enter with the help of keyboard
+let input = document.querySelector("input");
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        btn.click();
+    }
+});
